602a0b3d3a5f553e8f35639da8d5c6d7d6eaed8a
jwp345 <jwp135@naver.com>
2020-08-03 15:59:14 +0900

    emaillist03 완성

--- emaillist03/src/main/java/com/bit2020/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/com/bit2020/emaillist/controller/EmaillistController.java
@@ -1,15 +1,37 @@
 package com.bit2020.emaillist.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bit2020.emaillist.repository.EmaillistRepository;
+import com.bit2020.emaillist.vo.EmaillistVo;
 
 @Controller
 public class EmaillistController {
-
-	@ResponseBody
+	@Autowired
+	private EmaillistRepository emaillistRepository;
+	
 	@RequestMapping("")
-	public String index() {
+	public String index(Model model) {
+		List<EmaillistVo> list = emaillistRepository.findAll();
+		model.addAttribute("list", list);
 		return "/WEB-INF/views/index.jsp";
+	}
+	
+	@RequestMapping("/form")
+	public String form() {
+		return "/WEB-INF/views/form.jsp";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(EmaillistVo vo) {
+		emaillistRepository.insert(vo);
+		return "redirect:/";
 	}
 }
